847079de9e178a37eb1c5f1ea45270b9afb18813
Masata NISHIDA <masatanish@gmail.com>
2012-08-11 10:32:16 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whitelily::Application.config.secret_token = '09d7f0039f8e3d79b9362a337ccb68fcbca63f0f9a779cc5f674da90693c49922895f6832b4f1188e6745b0a2abbcbc78ac312a92e0f791297395f0f3995c805'
