a3d7c8db6520a0c1cc3c82d62c2043221b85f518
Masata NISHIDA <masatanish@gmail.com>
2012-08-12 18:50:31 +0900

    change redirect url at login, logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_path, notice: 'Login sccessful.'
+    redirect_to :back, notice: 'Login sccessful.'
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logout.'
+    redirect_to :back, notice: 'Logout.'
 
   end
   def failure
