c48426fd580c9c31a2e563ef982e64ca5902a4bf
Masata NISHIDA <masatanish@gmail.com>
2012-08-18 14:57:53 +0900

    implement issue#1

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,6 +5,9 @@ class Place < ActiveRecord::Base
   belongs_to :last_editor, :class_name => 'User'
 
   validates_format_of :tabelog_url, :with => /^http:\/\/tabelog\.com\/.*/, :allow_blank => true
+  validates_presence_of :name
+  validates_presence_of :address
+
 
 
   acts_as_gmappable :check_process => false
